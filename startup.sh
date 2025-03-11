64eab762dee3dbbf19e11fc2fae9892328633907
Isaac Khabra <80500622+isaackhabra@users.noreply.github.com>
2025-03-11 14:42:24 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget -O /home/e1-target/server.py "https://raw.githubusercontent.com/isaackhabra/test/refs/heads/main/server.py"
-python -m venv /etc/v
+python -m venv /home/e1-target/v
 python /home/e1-target/server.py
